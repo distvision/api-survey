3560ab369310663f2cda20b7499447369236355f
distvision <furede2@gmail.com>
2022-03-09 17:12:01 +0200

    Creating tests

--- api/src/__tests__/User.test.ts
+++ api/src/__tests__/User.test.ts
@@ -1,12 +1,32 @@
 import request from "supertest";
 import { app } from "../app";
 
+import createConnection from "../database"
 
-describe("User", ()=>{
 
-  request(app).post("/users")
+describe("Users", ()=>{
+  beforeAll(async ()=>{
+    const Connection = await createConnection()
+    await Connection.runMigrations()
+  })
+
+it("should be able to create a new user", async ()=>{
+  const response = await request(app).post("/users")
   .send({
     email : "user@example.com",
     name:  "User Example"
   })
+  
+  expect(response.status).toBe(201)
+})
+
+it("should not be able to create a user with exists email", async ()=>{
+  const response = await request(app).post("/users")
+  .send({
+    email : "user@example.com",
+    name:  "User Example"
+  })
+  
+  expect(response.status).toBe(400)
+})
 })
